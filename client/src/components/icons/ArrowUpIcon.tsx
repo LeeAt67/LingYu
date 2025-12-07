31371ba2e494eceb1a399da0afdcf28ae38cb706
LeeAt67 <1036232425@qq.com>
2025-12-07 17:07:54 +0800

    fix：修复SVG图标使用驼峰命名法

--- client/src/components/icons/ArrowUpIcon.tsx
+++ client/src/components/icons/ArrowUpIcon.tsx
@@ -6,16 +6,16 @@ interface ArrowUpIconProps {
 const ArrowUpIcon = ({ className = '', size = 24 }: ArrowUpIconProps) => {
     return (
         <svg  
-        height={size}
-        width={size}
-        className={className} 
-        xmlns="http://www.w3.org/2000/svg" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        stroke="currentColor" 
-        stroke-width="2" 
-        stroke-linecap="round" 
-        stroke-linejoin="round" 
+            height={size}
+            width={size}
+            className={className} 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
         >
             <path d="m5 12 7-7 7 7" />
             <path d="M12 19V5" />
